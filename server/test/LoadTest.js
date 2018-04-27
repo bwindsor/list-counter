7a179fbe04d1f8b64fca521e4c37089e4d753d21
Ben Windsor <benwindsor@gmail.com>
2018-04-27 15:19:09 +0100

    Make LoadTest a bit more formal

--- server/test/LoadTest.js
+++ server/test/LoadTest.js
@@ -1,10 +1,14 @@
 const fetch = require('node-fetch');
+var mocha = require('mocha');
+var assert = require('assert');
 
-let promises = [];
-for (let i = 0; i < 50; i++) {
-    promises.push(fetch("https://9se8n3sre0.execute-api.us-east-1.amazonaws.com/prod/item"));
-}
-Promise.all(promises)
-//.then(res => console.log(res.map(r=>r.status)));
-.then(res => Promise.all(res.map(r => r.json())))
-.then(json => console.log(json));
+describe('LoadTest', () => {
+    it('complains about too many requests', async () => {
+        let promises = [];
+        for (let i = 0; i < 100; i++) {
+            promises.push(fetch("https://9se8n3sre0.execute-api.us-east-1.amazonaws.com/prod/item"));
+        }
+        var res = await Promise.all(promises)
+        assert.notEqual(res.map(r => r.status).indexOf(429), -1)
+    }).timeout(10000);
+});
