72b6f3f25719c5d864d40cfc168becea260533c2
Ben Windsor <benwindsor@gmail.com>
2018-04-27 15:22:07 +0100

    Read api address from environment

--- server/test/LoadTest.js
+++ server/test/LoadTest.js
@@ -4,9 +4,11 @@ var assert = require('assert');
 
 describe('LoadTest', () => {
     it('complains about too many requests', async () => {
+        var API_BASE = process.env.API_BASE;
+
         let promises = [];
         for (let i = 0; i < 100; i++) {
-            promises.push(fetch("https://9se8n3sre0.execute-api.us-east-1.amazonaws.com/prod/item"));
+            promises.push(fetch(API_BASE + "/item"));
         }
         var res = await Promise.all(promises)
         assert.notEqual(res.map(r => r.status).indexOf(429), -1)
